449ecf0b691376d1a328beba1ce64d4a9f6be539
bongdong2 <jsu373712@naver.com>
2019-12-26 13:35:12 +0900

    HelloController, README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+Web service by Springboot & AWS
+=========
+
+## 개발환경
+- Mac OS
+- Intellij
+- java 8
+- gradle
+
--- src/test/java/me/seungui/meeting/web/HelloControllerTest.java
+++ src/test/java/me/seungui/meeting/web/HelloControllerTest.java
@@ -7,11 +7,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -28,5 +27,23 @@ public class HelloControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+
+        //http://localhost:8080/hello/
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 999;
+
+        mockMvc.perform(get("/hello/dto")
+                            .param("name", name)
+                            .param("amount", String.valueOf(amount)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
+        //http://localhost:8080/hello/dto?name=seungui&amount=30
     }
 }
